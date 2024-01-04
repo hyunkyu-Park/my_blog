81b2878a663de0b08c0d898da072eeb822d266f1
hyunkyu-Park <park620630@gmail.com>
2024-01-03 23:21:35 -0500

    buttons

--- src/App.js
+++ src/App.js
@@ -5,23 +5,37 @@ import { useState } from 'react';
 function App() {
 
   let post = '역삼 우동 맛집'
-  let [s1, b] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학'])
+  let [titles, t] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학'])
+  let [like, l] = useState(0);
   
   return (
     <div className="App">
       <div className="black-nav">
         <h4>블로그임</h4>
       </div>
+
+      <button onClick={() => {
+        let copy = [...titles];
+        copy[0] = '여자 코트 추천';
+        t(copy);
+      }}>성별 바꾸기</button>
+
+      <button onClick={() => {
+        let copy = [...titles];
+        copy.sort();
+        t(copy)
+      }}>가나다순 정렬</button>
+
       <div className="list"> 
-        <h4>{s1[0]}</h4>
+        <h4>{titles[0]} <span onClick={() => { l(like++) }}>👍</span> {like}  </h4>
         <p>1월 1일 발행</p>
       </div>
       <div className="list"> 
-        <h4>{s1[1]}</h4>
+        <h4>{titles[1]} <span>👍</span> 0 </h4>
         <p>1월 2일 발행</p>
       </div>
       <div className="list"> 
-        <h4>{s1[2]}</h4>
+        <h4>{titles[2]}</h4>
         <p>1월 3일 발행</p>
       </div>
     </div>
